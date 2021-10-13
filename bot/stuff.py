c6f64be1732b7197a16867949142f87488246c9b
Percy <percy@railway.app>
2021-10-13 08:06:48 +0000

    Initial Commit

---
+++ bot/stuff.py
@@ -0,0 +1,46 @@
+#    This file is part of the CompressorQueue distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
+
+from .worker import *
+
+async def up(event):
+    if not event.is_private:
+        return
+    stt = dt.now()
+    ed = dt.now()
+    v = ts(int((ed - uptime).seconds) * 1000)
+    ms = (ed - stt).microseconds / 1000
+    p = f"Ping = {ms}ms"
+    await event.reply(v + "\n" + p)
+
+
+async def start(event):
+    await event.reply(
+        f"**Send me the video which you want to compress.**",
+        buttons=[
+            [Button.inline("HELP", data="ihelp")],
+        ],
+    )
+
+async def help(event):
+    await event.reply(
+        f"""**To check current ffmpeg command you can use.**\n\n`/eval print(ffmpegcode[0])`\n\n**You can change your ffmpeg code by executing following commands.**\n\n**➩** `/eval ffmpegcode.clear()`\n\n**➩** `/eval ffmpegcode.insert(0, "-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By Zylern' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2  -ab 32k  -vbr 2 -level 3.1")`\n\n**Don't use \" inside your ffmpeg code it will give you errors use \' instead.**"""
+    )
+
+
+async def ihelp(event):
+    await event.edit(
+        """**To check current ffmpeg command you can use.**\n\n`/eval print(ffmpegcode[0])`\n\n**You can change your ffmpeg code by executing following commands.**\n\n**➩** `/eval ffmpegcode.clear()`\n\n**➩** `/eval ffmpegcode.insert(0, "-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By Zylern' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2  -ab 32k  -vbr 2 -level 3.1")`\n\n**Don't use \" inside your ffmpeg code it will give you errors use \' instead.**"""
+    )
